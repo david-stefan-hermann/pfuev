f4c3b27050bf68c2c2cf90b780fd26025004bed1
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-06-28 20:04:16 +0200

    added Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <div>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+        </div>
+    )
+}
+
+export default Navbar
