7c68299ea6da7273f3f9164c303227d1a8324c7c
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-06-29 02:37:35 +0200

    added components

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,15 @@
 import Navbar from "./Navbar"
+import HeroCarousel from "./HeroCarousel"
+import Footer from "./Footer"
+import HeaderSizedBox from "./HeaderSizedBox"
+import Statistics from "./Statistics"
+import Testimonials from "./Testimonials"
 
-export { Navbar }
+export {
+    Navbar,
+    HeroCarousel,
+    Footer,
+    HeaderSizedBox,
+    Statistics,
+    Testimonials,
+}
