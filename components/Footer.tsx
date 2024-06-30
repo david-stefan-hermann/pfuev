47e55d22c0f28cc124507055b0aeb1fb126f69d1
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-06-30 19:50:58 +0200

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,91 @@
+'use client'
+
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+    VisuallyHidden,
+    Image
+} from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+const Logo: React.FC = (props: any) => {
+    return (
+        <Image src="/logo.png" height={"20"} alt="Pfüv Logo" />
+    )
+}
+
+const SocialButton: React.FC<{ children: ReactNode, label: string, href: string }> = ({ children, label, href }) => {
+    return (
+        <chakra.button
+            bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+            rounded={'full'}
+            w={8}
+            h={8}
+            cursor={'pointer'}
+            as={'a'}
+            href={href}
+            display={'inline-flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            transition={'background 0.3s ease'}
+            _hover={{
+                bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+            }}>
+            <VisuallyHidden>{label}</VisuallyHidden>
+            {children}
+        </chakra.button>
+    )
+}
+
 const Footer: React.FC = () => {
     return (
-        <footer>
-            <p>© 2021 My Company</p>
-        </footer>
+        <Box
+            bg={useColorModeValue('gray.100', 'gray.700')}
+            color={useColorModeValue('gray.700', 'gray.200')}>
+            <Container
+                as={Stack}
+                maxW={'6xl'}
+                py={4}
+                spacing={4}
+                justify={'center'}
+                align={'center'}>
+                <Logo />
+                <Stack direction={'row'} spacing={6}>
+                    <Box as="a" href={'/#'}>
+                        Home
+                    </Box>
+                    <Box as="a" href={'/#pfuev'}>
+                        Über uns
+                    </Box>
+                    <Box as="a" href={'/#partner'}>
+                        Unsere Partner
+                    </Box>
+                    <Box as="a" href={'/#kontakt'}>
+                        Kontakt
+                    </Box>
+                </Stack>
+            </Container>
+
+            <Box
+                borderTopWidth={1}
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.300', 'gray.700')}>
+                <Container
+                    as={Stack}
+                    maxW={'6xl'}
+                    py={4}
+                    direction={{ base: 'column', md: 'row' }}
+                    spacing={4}
+                    justify={{ base: 'center', md: 'space-between' }}
+                    align={{ base: 'center', md: 'center' }}>
+                    <Text>© 2024 PFÜV - Pferdischer Überwachungsverein</Text>
+                </Container>
+            </Box>
+        </Box>
     )
 }
 
