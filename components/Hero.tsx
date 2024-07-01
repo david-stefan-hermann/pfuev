721620b745ffbaa52076f288d81432f893727306
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-07-01 17:43:07 +0200

    100vh -> 90vh

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ import {
 
 const Hero: React.FC = () => {
     return (
-        <Stack minH='100vh' direction={{ base: 'column', md: 'row' }}>
+        <Stack minH='90vh' direction={{ base: 'column', md: 'row' }}>
             <Flex p={8} flex={1} align='center' justify='center'>
                 <Stack spacing={6} w='full' maxW='lg'>
                     <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
