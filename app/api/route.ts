ff80fd91e1755fef0dbdf6ea5076c29c21e37199
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-07-01 21:49:03 +0200

    mail api works, using cf env variables rn

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,14 +3,20 @@ import { Resend } from 'resend'
 export async function POST(request: Request, env: { RESEND_API_KEY: string }) {
     const { name, email, message } = await request.json()
 
-    const resend = new Resend(env.RESEND_API_KEY || "undefined");
+    const resend = new Resend(env.RESEND_API_KEY || "x")
 
     const { data, error } = await resend.emails.send({
-        from: email || 'Keine Email angegeben',
+        from: 'info@pfuev.org',
         to: 'info@pfuev.org',
-        subject: 'Pfuev.org: Neue Nachricht von ' + name || 'Unbekannt',
-        html: message || 'Keine Nachricht',
+        subject: 'Pfuev.org: Nachricht von ' + name || 'Unbekannt',
+        html: `
+        <h3>Nachricht</h3>
+        <p>${message || 'keine Nachricht'}</p>
+        <h3>Absender</h3>
+        <p>Name: ${name || 'kein Name'}</p>
+        <p>Email: ${email || 'keine Email'}</p>`,
     })
+    console.log(data, error)
     return Response.json({ data, error })
 }
 
