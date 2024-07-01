d91778b511e805f13871fc4b3edb74fbd0c07a65
david-stefan-hermann <david.stefan.hermann@gmail.com>
2024-07-01 21:06:12 +0200

    resend email integration in backend

---
+++ app/api/route.ts
@@ -0,0 +1,16 @@
+import { Resend } from 'resend'
+
+export async function POST(request: Request, env: { RESEND_API_KEY: string }) {
+    const { name, email, message } = await request.json()
+
+    const resend = new Resend(env.RESEND_API_KEY || "undefined");
+
+    const { data, error } = await resend.emails.send({
+        from: email || 'Keine Email angegeben',
+        to: 'info@pfuev.org',
+        subject: 'Pfuev.org: Neue Nachricht von ' + name || 'Unbekannt',
+        html: message || 'Keine Nachricht',
+    })
+    console.log(env.RESEND_API_KEY)
+    return Response.json({ data, error })
+}
